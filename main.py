2d0c677ea8c923ce6d17ef51dc660f83c781a61a
hearthlessnoob <144697818+hearthlessnoob@users.noreply.github.com>
2023-09-28 11:24:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 #importer un module
 import random
 
-#fonction pour demander les bornes du joueur et choisir un nb random entre les bornes, input: integer, ouput: integer
+#fonction pour deemander les bornes du joueur et choisir un nb random entre les bornes, input: integer, ouput: integer
 def new_nb():
     range_min = int(input("Qu'elle est le borne minimum:"))
     range_max = int(input("Qu'elle est le borne maximum:"))
     nb = random.randint(range_min, range_max)
     print("J’ai choisi un nombre au hasard entre %d et %d."%(range_min, range_max))
     print("À vous de le deviner...")
+    print(nb)
     return nb
 nb = new_nb()
 nb_tries = 0
+replay = True
 #main loop pour que le joueur peut jouer plusieurs fois
-while True:
+while replay:
    guess = int(input("Entrez votre essai : "))
    nb_tries += 1
    #regarder si le nb que le joueur a entré est trop grand, trop petit ou si il a deviné le bon nb
@@ -25,7 +27,7 @@ while True:
            nb = new_nb()
            nb_tries = 0
        else:
-           break
+           replay = False
    elif guess > nb:
        print("Mauvais choix, le nombre est plus petit que %d." % guess)
    else:
